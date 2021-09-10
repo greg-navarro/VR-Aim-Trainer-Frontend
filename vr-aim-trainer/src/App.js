bfd709e42a73d1d05de1a57c470061d3db88a25b
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-10 12:41:52 -0700

    Try out bootstrap navbar

--- vr-aim-trainer/src/App.js
+++ vr-aim-trainer/src/App.js
@@ -1,16 +1,29 @@
 import {BrowserRouter as Router} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
 // import components
-import Navbar from 'react-bootstrap/Navbar';
+// import Navbar from './components/navbar.component';
 
 
 function App() {
   return (
     <Router>
       <div className="container-fluid">
-      <Navbar />
+      <Navbar>
+        <Container fluid>
+          <Navbar.Brand href="#home">Navbar with text</Navbar.Brand>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
         <p>Yo yo yo</p>
       </div>
     </Router>
