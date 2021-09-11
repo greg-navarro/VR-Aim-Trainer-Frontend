5632b9246b6dddaab5b7117b655a74a08893d2af
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-11 12:39:49 -0700

    Switch to full react bootstrap

--- vr-aim-trainer/src/components/navbarcomp.component.js
+++ vr-aim-trainer/src/components/navbarcomp.component.js
@@ -1,8 +1,9 @@
   
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+
 
 import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 // note that the export statement is made in the declaration instead of at the end
@@ -10,11 +11,15 @@ export default class NavbarComp extends Component {
 
 	render() {
 		return (
-			<Navbar>
+			<Navbar bg="dark" variant="dark">
 				<Container fluid>
-					<Link to="/" className="navbar-brand">VR Aim Trainer</Link>
-					<Link to="/leaderboard" className="">Leaderboard</Link>
-					<Link to="/contact" className="">Contact</Link>
+				<Navbar.Brand href="#home">VR Aim Trainer</Navbar.Brand>
+				<Nav className="me-auto">
+					<Nav.Link href="#home">Home</Nav.Link>
+					<Nav.Link href="#leaderboard">Leaderboard</Nav.Link>
+					<Nav.Link href="#about">About</Nav.Link>
+					<Nav.Link href="#contact">Contact</Nav.Link>
+				</Nav>
 				</Container>
 			</Navbar>
 			)
