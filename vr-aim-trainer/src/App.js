4fa4cfc9eb56d3066937c382f2ccafa6c693b0b0
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-11 18:09:29 -0700

    Add route for home page component

--- vr-aim-trainer/src/App.js
+++ vr-aim-trainer/src/App.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarComp from "./components/navbarcomp.component";
 import Leaderboard from "./components/leaderboard.component";
+import Home from "./components/home.component";
 // import components
 // import Navbar from './components/navbar.component';
 
@@ -17,6 +18,7 @@ function App() {
       <div className="container-fluid">
         <NavbarComp />
         <br />
+        <Route path="/" exact component={Home} />  
         <Route path="/leaderboard" component={Leaderboard} />
       </div>
      
